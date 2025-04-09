2407189756feb4b004f16815da9ffd6a97d89213
Olya Dubodel <dubodel.olya@gmail.com>
2025-04-09 18:03:34 +0300

    feat: solve queuePromises task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -134,8 +134,16 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  let resultPromise = Promise.resolve('');
+  for (let i = 0; i < promises.length; i += 1) {
+    resultPromise = resultPromise.then((resPromVal) => {
+      return promises[i].then((promise) => {
+        return resPromVal + promise;
+      });
+    });
+  }
+  return resultPromise;
 }
 
 module.exports = {
